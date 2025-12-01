3651a2927130eb33281e7a22afd793b04fb406fa
Jatin Kumar <jatin@possibilitysolutions.com>
2025-12-01 18:39:29 +0530

    update the screenshots

--- README.md
+++ README.md
@@ -251,11 +251,11 @@ Here are some screenshots of the example app demonstrating the key features of t
 
 ### Screenshot 2
 
-<img src="https://raw.githubusercontent.com/PbsTeam/Provider_feature_first/main/assets/screenshots/screenshots_01.png" alt="Home Screen" width="300"/>
+<img src="https://raw.githubusercontent.com/PbsTeam/Provider_feature_first/main/assets/screenshots/screenshots_04.png" alt="Home Screen" width="300"/>
 
 ### Screenshot 3
 
-<img src="https://raw.githubusercontent.com/PbsTeam/Provider_feature_first/main/assets/screenshots/screenshots_02.png" alt="Home Screen" width="300"/>
+<img src="https://raw.githubusercontent.com/PbsTeam/Provider_feature_first/main/assets/screenshots/screenshots_05.png" alt="Home Screen" width="300"/>
 
 
 ### Screenshot 4
--- lib/features/splash/data/repository/splash_repository.dart
+++ lib/features/splash/data/repository/splash_repository.dart
@@ -6,17 +6,17 @@ import '../../../../routes/routes_names.dart';
 class SplashRepo {
   isLogin(BuildContext context) {
     Timer(Duration(seconds: 3), () {
-      // LocalStorage.instance.isLogin
-      //     ? Navigator.pushNamedAndRemoveUntil(
-      //         context,
-      //         RoutesName.homeScreen,
-      //         (route) => false,
-      //       )
-      //     : Navigator.pushNamedAndRemoveUntil(
-      //         context,
-      //         RoutesName.loginScreen,
-      //         (route) => false,
-      //       );
+      LocalStorage.instance.isLogin
+          ? Navigator.pushNamedAndRemoveUntil(
+              context,
+              RoutesName.homeScreen,
+              (route) => false,
+            )
+          : Navigator.pushNamedAndRemoveUntil(
+              context,
+              RoutesName.loginScreen,
+              (route) => false,
+            );
     });
   }
 }
